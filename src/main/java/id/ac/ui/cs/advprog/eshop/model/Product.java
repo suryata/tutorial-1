7a9d42bcef7182bb069aa00e10c59add2b750ac0
suryata <madeputra415@gmail.com>
2024-02-05 19:08:19 +0800

    delete sudah berhasil diimplementasikan

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -2,10 +2,15 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import java.util.UUID;
 
 @Getter @Setter
 public class Product {
     private String productID;
     private String productName;
     private int productQuantity;
+
+    public Product() {
+        this.productID = UUID.randomUUID().toString();
+    }
 }
