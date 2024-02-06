3bb2f2a3713de3f95519cf8b182d77a71882544a
suryata <madeputra415@gmail.com>
2024-02-06 21:33:46 +0800

    HomepageFunctionalTest is done

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
@@ -32,7 +32,7 @@ class HomePageFunctionalTest {
     @Test
     void pageTitle_isCorrect(ChromeDriver driver) throws Exception{
         //Exercise
-        driver.get(baseUrl + "/product/list");
+        driver.get(baseUrl);
         String pageTitle = driver.getTitle();
 
         //Verify
@@ -42,7 +42,7 @@ class HomePageFunctionalTest {
     @Test
     void welcomeMessage_homePage_isCorrect(ChromeDriver driver) throws Exception{
         //Exercise
-        driver.get(baseUrl + "/product/list");
+        driver.get(baseUrl);
         String welcomeMessage = driver.findElement(By.tagName("h3")).getText();
 
         //Verify
