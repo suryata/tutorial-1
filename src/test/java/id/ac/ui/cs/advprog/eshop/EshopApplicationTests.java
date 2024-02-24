f6eb065deed0ae900a2da389e3a552fa4d401bd2
suryata <madeputra415@gmail.com>
2024-02-24 15:38:57 +0700

    new test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,24 +1,21 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class EshopApplicationTests {
+public class EshopApplicationTests {
 
     @Autowired
     private ApplicationContext context;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
         assertThat(context).isNotNull();
     }
 
-	@Test
-    void main() {
-        EshopApplication.main(new String[]{});
-    }
 }
