6738f6c3531d75e8515cd88523a5a6d16b8c0e0b
suryata <madeputra415@gmail.com>
2024-02-24 15:35:10 +0700

    fixing readme

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,24 +1,24 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class EshopApplicationTests {
+class EshopApplicationTests {
 
     @Autowired
     private ApplicationContext context;
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
         assertThat(context).isNotNull();
     }
 
 	@Test
-    public void main() {
+    void main() {
         EshopApplication.main(new String[]{});
     }
 }
