f1a1cf8535bc0a6edffaaf55e914b281f536be5e
suryata <madeputra415@gmail.com>
2024-02-24 16:31:34 +0700

    Deleting some test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,13 @@
-package id.ac.ui.cs.advprog.eshop;
+// package id.ac.ui.cs.advprog.eshop;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class EshopApplicationTests {
+// @SpringBootTest
+// public class EshopApplicationTests {
 
-    @Test
-    void applicationContextTest() {
-        EshopApplication.main(new String[]{});
-    }
-}
+//     @Test
+//     void applicationContextTest() {
+//         EshopApplication.main(new String[]{});
+//     }
+// }
