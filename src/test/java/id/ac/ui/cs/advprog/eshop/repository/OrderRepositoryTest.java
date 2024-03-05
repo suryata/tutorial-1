4d8a21a6d0261a71b7960b3feded19f75aaa9c48
suryata <madeputra415@gmail.com>
2024-03-05 18:33:17 +0700

    [RED] Add OrderRepository skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -23,7 +23,7 @@ class OrderRepositoryTest {
         List <Product> products = new ArrayList<>();
 
         Product product1 = new Product();
-        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductID("eb558e9f-1c39-460e-8860-71af6af63bd6");
         product1.setProductName("Sampo Cap Bambang");
         product1.setProductQuantity(2);
         products.add(product1);
