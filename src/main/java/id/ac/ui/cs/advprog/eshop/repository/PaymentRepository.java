3e27e366e89621d77e28d2bca6f7285200053516
suryata <madeputra415@gmail.com>
2024-03-06 07:40:31 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -10,17 +10,28 @@ import java.util.ArrayList;
 @Repository
 public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
-    
+
     public Payment save(Payment payment) {
-        return null;
+        for (Payment existingPayment : paymentData) {
+            if (existingPayment.getId().equals(payment.getId())) {
+                throw new IllegalStateException("Payment with the same ID already defined");
+            }
+        }
+        paymentData.add(payment);
+        return payment;
     }
 
     public List<Payment> getAllPayments() {
-        return null;
+        return new ArrayList<>(paymentData);
     }
 
     public Payment findById(String id) {
+        for (Payment payment : paymentData) {
+            if (payment.getId().equals(id)) {
+                return payment;
+            }
+        }
         return null;
     }
-    
+
 }
